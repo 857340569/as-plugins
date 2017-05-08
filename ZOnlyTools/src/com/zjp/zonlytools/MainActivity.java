3ac339438c05f8a4d763741223f9b83458d148bb
tb4631895 <tb4631895@2a2a2a5b-ddb6-4a68-bdbf-4a716dc13592>
2017-05-08 15:43:30 +0000

    接收短信什么的

---
+++ ZOnlyTools/src/com/zjp/zonlytools/MainActivity.java
@@ -0,0 +1,48 @@
+package com.zjp.zonlytools;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+	private TextView content;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		content=(TextView) findViewById(R.id.content);
+		IntentFilter intentFilter = new IntentFilter();
+		intentFilter.setPriority(IntentFilter.SYSTEM_HIGH_PRIORITY);
+		intentFilter.addAction("android.provider.Telephony.SMS_RECEIVED");
+		registerReceiver(new BroadcastReceiver() {
+			
+			@Override
+			public void onReceive(Context arg0, Intent arg1) {
+				 StringBuilder sb = new StringBuilder();  
+		            Bundle bundle = arg1.getExtras();  
+		            if(bundle!=null){  
+		                Object[] pdus = (Object[])bundle.get("pdus");  
+		                SmsMessage[] msg = new SmsMessage[pdus.length];  
+		                for(int i = 0 ;i<pdus.length;i++){  
+		                    msg[i] = SmsMessage.createFromPdu((byte[])pdus[i]);  
+		                }  
+		                  
+		                for(SmsMessage curMsg:msg){  
+		                    sb.append("You got the message From:【");  
+		                    sb.append(curMsg.getDisplayOriginatingAddress());  
+		                    sb.append("】\nContent：");  
+		                    sb.append(curMsg.getDisplayMessageBody());  
+		                }  
+		                content.setText(sb.toString()); 
+		                    
+		            }  
+			}
+		}, intentFilter);
+	}
+}
