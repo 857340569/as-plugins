17a9bb6c283c3d77b2490ede407f30418260c214
tb4631895 <tb4631895@2a2a2a5b-ddb6-4a68-bdbf-4a716dc13592>
2017-07-07 10:47:37 +0000

    app热修复fixed

--- MyHotfix/app/src/main/java/zp/myhotfix/SecondActivity.java
+++ MyHotfix/app/src/main/java/zp/myhotfix/SecondActivity.java
@@ -1,15 +1,29 @@
 package zp.myhotfix;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.SpannableStringBuilder;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import zp.myhotfix.utils.HotFixEngine;
+import zp.myhotfix.utils.TextUtils;
+import zp.myhotfix.view.SpanableStringTextView;
 
 public class SecondActivity extends Activity {
     private TextView show_info_view;
+    private ListView listview;
+    private String teststr="balabalaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaddddddddddddddddddbalabalaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaddddddddddddddddddddddddddddddd";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +33,47 @@ public class SecondActivity extends Activity {
     }
     private void initViews(){
         show_info_view = findView(R.id.show_info_view);
+        listview = findView(R.id.listview);
 //        show_info_view.setText("I'm bugs!");
-        show_info_view.setText("I'm fixed!");
+        show_info_view.setText("I'm fixed! ok");
+        listview.setAdapter(new BaseAdapter() {
+            @Override
+            public int getCount() {
+                return 10;
+            }
+
+            @Override
+            public Object getItem(int position) {
+                return null;
+            }
+
+            @Override
+            public long getItemId(int position) {
+                return 0;
+            }
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                convertView=LayoutInflater.from(SecondActivity.this).inflate(R.layout.item,null);
+                SpanableStringTextView textView= (SpanableStringTextView) convertView.findViewById(R.id.tv_detail);
+                SpannableStringBuilder style= TextUtils.getTextStyleBuilder(new ClickableSpan() {
+                    @Override
+                    public void onClick(View widget) {
+                        Toast.makeText(SecondActivity.this,"clicked",Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void updateDrawState(TextPaint ds) {
+                        //super.updateDrawState(ds);
+                        ds.setColor(Color.RED);
+                        ds.setUnderlineText(false);
+                    }
+                },position+teststr,"bala");
+                textView.setText(style);
+                textView.setMovementMethod(LinkMovementMethod.getInstance());
+                return convertView;
+            }
+        });
     }
 
 
